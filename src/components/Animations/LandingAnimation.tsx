43254c6280de4300065821acb4b7ba0d0fd7db33
natlou <natloudev@gmail.com>
2022-07-03 21:16:12 -0700

    solved type issues

--- src/components/Animations/LandingAnimation.tsx
+++ src/components/Animations/LandingAnimation.tsx
@@ -5,9 +5,14 @@ import { useEffect, useRef, useState } from 'react';
 
 const LandingAnimation = () => {
 
+    const canvas = document.getElementById('main') as HTMLCanvasElement | null;
+
+    const CTX = canvas?.getContext('2d');
+
+
+
     return (
-        <>
-        </>
+        <canvas id='main'></canvas>
     )
 }
 
