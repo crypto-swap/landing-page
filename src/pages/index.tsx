56a62a0f3203a2de358fbbe7d414cd9dab9250b1
natlou <natloudev@gmail.com>
2022-07-03 17:41:16 -0700

    removed scss file added react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,56 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import React from 'react';
 import Image from 'next/image';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Link from 'next/link';
-import animate from '../styles/HomeAnimation.module.scss'
+import animation from '../styles/HomeAnimation.module.scss'
 
 const styles = {
-  wrapper : 'flex justify-center align-center p-8',
+  wrapper : 'flex justify-center items-center p-8',
   zksyncButton : 'bg-[rgb(96,9,240)] bg-gradient-to-r from-[rgba(96,9,240,1)] to-[rgba(129,5,240,1)] p-5 rounded-2xl shadow-purple-gradient-button hover:shadow-purple-gradient-button-hover',
   buttonText: 'relative block h-full w-full',
+  animationWrapper : 'flex justify-center items-center flex-row flex-wrap mt-5'
 }
 
 const Home: NextPage = () => {
-    return (
-      <div className="relative h-screen overflow-x-hidden bg-bg-light font-[Montserrat] text-text-light dark:bg-bg-dark dark:text-text-dark">
-        <Head>
-          <title>CryptoSwap</title>
-          <meta name="description" content="CryptoSwap is a very efficient simple DEX on zkSync (Swap, Pool, Liquidity Options, and Generate). No presale, fully decentralized, and community-driven." />
-          <meta key="twitter:description" name="twitter:description" content="CryptoSwap" />
-          <link rel="icon" href="/icons/black_logo.svg" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="canonical" href="https://cryptoswap.org" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-  
-        <Navbar />
-  
-        <div className={styles.wrapper}>
-          <Link href='https://zksync.cryptoswap.org'>
-            <button className={styles.zksyncButton}>
-              <span className={styles.buttonText}>
-                Click this button to access zkSync Crypto Swap 
-              </span>
-            </button>
-          </Link>
-        </div>
 
-        <Footer />
+  return (
+    <div className="relative h-screen overflow-x-hidden bg-bg-light font-[Montserrat] text-text-light dark:bg-bg-dark dark:text-text-dark">
+      <Head>
+        <title>CryptoSwap</title>
+        <meta name="description" content="CryptoSwap is a very efficient simple DEX on zkSync (Swap, Pool, Liquidity Options, and Generate). No presale, fully decentralized, and community-driven." />
+        <meta key="twitter:description" name="twitter:description" content="CryptoSwap" />
+        <link rel="icon" href="/icons/black_logo.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="canonical" href="https://cryptoswap.org" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Navbar />
+
+      <div className={styles.wrapper}>
+        <Link href='https://zksync.cryptoswap.org'>
+          <button className={styles.zksyncButton}>
+            <span className={styles.buttonText}>
+              Click this button to access zkSync Crypto Swap 
+            </span>
+          </button>
+        </Link>
+
+        <div className={styles.animationWrapper}>
+          <canvas id="main"></canvas>
+        </div>
       </div>
-    );
-  };
-  
+
+      <Footer />
+    </div>
+  );
+};
+
   export default Home;
   
